f783b3ba16c5d86d3a91154ec2304d5edba00c2a
s019672 <s019672@lm-24s019672.lmsd.org>
2022-03-24 11:50:03 -0400

    Grant Curry

--- chess/src/King.java
+++ chess/src/King.java
@@ -1,4 +1,8 @@
-
+//project name: King.java
+//written by: Asher Polsky, Grant Curry, Nate Grunfeld
+//date: Mar 22, 2022
+//description: King Piece extends the piece class, determines if a move is valid 
+//based on if the piece moves 1 space and if it moves into check or not
 public class King extends Piece {
 
 	public King(int p) {
@@ -8,9 +12,16 @@ public class King extends Piece {
 		this(1);
 	}
 	
+	//method: isValidMove
+	//parameters: Location from, Location to, Piece[][]b
+	//return: boolean
+	//description: checks to see if the move a player wants to make is valid
+	//based on the king's movement behavior and if it moves into check or not then sets x and y coordinates to new values.
+	//returns true if move was valid, and false if it was not valid.
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
 		return super.isValidMove(from, to, b) && super.isValidKingMove(from, to, b) && super.doesItGoIntoCheck(from, to, b);
 	}
+	
 	public String toString(){
 		return "K";
 	}
--- chess/src/Knight.java
+++ chess/src/Knight.java
@@ -1,4 +1,8 @@
-
+//project name: Bishop.java
+//written by: Asher Polsky, Grant Curry, Nate Grunfeld
+//date: Mar 22, 2022
+//description: Knight Piece extends the piece class and determines if a move is valid 
+//based on if the knights movement behavior
 public class Knight extends Piece {
 	public Knight(int p){
 		super(p, 5, "knight");
@@ -8,8 +12,12 @@ public class Knight extends Piece {
 		this(1);
 	}
 	
-	// check to see if the move is valid, and then sets x and y coordinates to new values.
-	// returns true if move was valid, and false if it was not valid.
+	//method: isValidMove
+	//parameters: Location from, Location to, Piece[][]b
+	//return: boolean
+	//description: checks to see if the move a player wants to make is valid
+	//based on the knight's movement behavior then sets x and y coordinates to new values.
+	//returns true if move was valid, and false if it was not valid.
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
 		return super.isValidMove(from, to, b) && (isValidKnightMove(from, to, b));
 	}
